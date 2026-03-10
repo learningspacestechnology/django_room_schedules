2aa29c80e6530d7d209de10e57b8ecda36598b43
dunnmj <dunnmj@users.noreply.github.com>
2026-03-10 21:09:02 +0000

    feat: swap to unfold admin

--- admin.py
+++ admin.py
@@ -1,7 +1,25 @@
 from django.contrib import admin
+from unfold.admin import ModelAdmin, TabularInline
 
-# Register your models here.
 from room_schedules.models import Venue, Room
 
-admin.site.register(Venue)
-admin.site.register(Room)
+
+class RoomInline(TabularInline):
+    model = Room
+    extra = 0
+    fields = ('name', 'artifax_id')
+
+
+@admin.register(Venue)
+class VenueAdmin(ModelAdmin):
+    list_display = ('name', 'artifax_id')
+    search_fields = ('name',)
+    inlines = [RoomInline]
+
+
+@admin.register(Room)
+class RoomAdmin(ModelAdmin):
+    list_display = ('name', 'venue', 'artifax_id')
+    search_fields = ('name', 'venue__name')
+    list_filter = ('venue',)
+    list_select_related = ('venue',)
